5f896de7e31b4a555a183109fe5329b4487c7edc
Matthias Cullmann <matthias.cullmann@baloise.ch>
2017-07-03 15:23:15 +0200

    signers param

---
+++ src/main/java/com/baloise/confluence/digitalsignature/DigitalSignatureMacro.java
@@ -0,0 +1,30 @@
+package com.baloise.confluence.digitalsignature;
+
+import java.util.Map;
+
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.macro.Macro;
+import com.atlassian.confluence.macro.MacroExecutionException;
+
+public class DigitalSignatureMacro implements Macro {
+
+	@Override
+	public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
+		 if (map.get("signers") != null) {
+		        return ("<h1>Hello " + map.get("signers") + "!</h1>");
+		    } else {
+		        return "<h1>Hello World!<h1>";
+		    }
+	}
+
+	@Override
+	public BodyType getBodyType() {
+		 return BodyType.NONE; 
+	}
+
+	@Override
+	public OutputType getOutputType() {
+		 return OutputType.BLOCK;
+	}
+
+}
