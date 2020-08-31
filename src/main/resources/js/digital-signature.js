8ee06c4fbf7f08fc0d15fd16a821821d1a2eb559
Markus Lindenmann <markus.lindenmann@baloise.ch>
2020-08-31 13:48:42 +0200

    #26: fixes visibility and function of "show more" button

--- src/main/resources/js/digital-signature.js
+++ src/main/resources/js/digital-signature.js
@@ -5,11 +5,12 @@ function bindCollapse(ul, limit, showMore) {
 
     let $ul = AJS.$(ul);
 
-    hideElements($ul, limit);
-    $ul.prepend("<li><a href='javascript:void(0);'>" + showMore + "</a></li>")
-       .bind("click", function() {
-           showAllElements(this);
-       });
+    if (hideElements($ul, limit)) {
+        $ul.prepend("<li class='show-all'><a href='javascript:void(0);'>" + showMore + "</a></li>")
+            .bind("click", function () {
+                showAllElements($ul);
+            });
+    }
 }
 
 function hideElements($ul, limit) {
@@ -18,21 +19,25 @@ function hideElements($ul, limit) {
 
     let remainingCount = limit;
 
+    let isSomethingHidden = false;
     if (signedList.length > 0) {
         let shownSignees = Math.min(signedList.length, Math.ceil(remainingCount / 2));
         remainingCount = remainingCount - shownSignees;
         for (let i = 0; i < signedList.length - shownSignees; i++) {
-            AJS.$(signedList[i]).hide();
+            AJS.$(signedList[i]).hide()
+            isSomethingHidden = true;
         }
     }
 
     for (let i = 0; i < missingList.length - remainingCount; i++) {
         AJS.$(missingList[i]).hide();
+        isSomethingHidden = true;
     }
+
+    return isSomethingHidden;
 }
 
-function showAllElements(button) {
-    let $button = AJS.$(button);
-    $button.closest("ul").find("li").show();
-    $button.remove();
+function showAllElements($ul) {
+    $ul.find("li").show();
+    $ul.find("li.show-all").remove();
 }
